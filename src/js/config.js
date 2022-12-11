ea35ee8b4002c32b50d80ffd61102343624b063f
samogoljat <samo.goljat1@gmail.com>
2022-12-11 15:31:15 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'b5416182-ee6d-4819-b3f9-7e2ea5b7d730';
+export const MODAL_CLOSE_SEC = 2.5;
